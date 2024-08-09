710ca98c9dcc0c290f6bb9ce1fed3bcc446bc7ba
Bibekananda Hati <bibekanandahati75@gmail.com>
2024-08-09 16:56:55 +0530

    jkn

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Torch-PipeLine',
+    name='PyTorchLabFlow',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
@@ -15,7 +15,7 @@ setup(
     description='A pipeline for managing PyTorch models, datasets, optimizers, and more.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/BBEK-Anand/Torch-PipeLine',
+    url='https://github.com/BBEK-Anand/PyTorchLabFlow',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
