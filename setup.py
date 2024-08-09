85478755ba3fc67a451ec25f86692ea44fa201f0
Bibekananda Hati <bibekanandahati75@gmail.com>
2024-08-09 14:04:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Torch-PipeLine',
     version='0.1.0',
-    packages=['.'],
+    packages=find_packages(),
     install_requires=[
         'torch',
         'librosa',
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/BBEK-Anand/Torch-PipeLine',
     classifiers=[
         'Programming Language :: Python :: 3',
-
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.1',
